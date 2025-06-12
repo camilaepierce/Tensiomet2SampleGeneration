5eeee444bff477b2cc0cb645dd8f49c9be877c97
camilaepierce <133548541+camilaepierce@users.noreply.github.com>
2025-06-12 13:26:15 +0200

    added parameters_simple

---
+++ parameters_simple.m
@@ -0,0 +1,66 @@
+% % physical parameters for the simple droplet problem
+% params_phys.sigma = 4;      % surface tension
+% params_phys.grav = 1.2;     % gravitational acceleration
+% params_phys.rneedle = 1.4;  % radius of the needle
+% params_phys.volume0 = 16;   % prescribed volume
+% params_phys.deltarho = 1.1; % density difference
+% 
+% % Worthington number (needed for initial shape guess)
+% params_phys.Wo = params_phys.deltarho*params_phys.grav*...
+%     params_phys.volume0/(2*pi*params_phys.sigma*params_phys.rneedle);
+
+
+% Commented out the above, the belox is copied from
+% parameters_simple_manuscript
+
+% physical parameters for the simple droplet problem
+
+% RANDOMIZING PARAMETERS
+% Set random seed (seed 1 or default)
+
+% Set min/max of parameters for random variables
+Wo_min = 1;
+Wo_max = 10;
+Ar_min = 1;
+Ar_max = 20;
+    % % "Wo_paper": 1.9598749316252596,
+    % % "Ar_paper": 19.27606353624602,
+    % "Wo": 0.1657929801968887,
+    % "Wo_paper": 1.041708017206607,
+    % "Ar_paper": 15.723298829518545,
+
+% dimensionfull input parameters
+Wo = (rand * (Wo_max - Wo_min)) + Wo_min;
+Ar = (rand * (Ar_max - Ar_min)) + Ar_min;
+sigma = 5;        % surface tension [mN/m]
+grav = 9.807e3;    % gravitational acceleration [mm/s^2]
+deltarho = 1e-3;   % density difference [10^6 kg/m^3]
+
+% calculation of dimensionfull parameters
+rneedle = sqrt((Wo*sigma/(deltarho*abs(grav)*Ar))); % radius of the needle [mm]
+volume0 = Ar*rneedle^3;  % prescribed volume in mm^3
+
+% dimensional input parameters, for saving
+params_phys.sigma_dimal = sigma;
+params_phys.grav_dimal = grav;
+params_phys.rneedle_dimal = rneedle;
+params_phys.volume0_dimal = volume0;
+params_phys.deltarho_dimal = deltarho;
+
+% dimensionless input parameters for calculation
+params_phys.sigma = sigma/(deltarho*abs(grav)*rneedle^2); %%% save
+params_phys.grav = grav/abs(grav);
+params_phys.rneedle = rneedle/rneedle;
+params_phys.volume0 = volume0/rneedle^3;
+params_phys.deltarho = deltarho/deltarho;
+
+% Worthington number (needed for initial shape guess)
+params_phys.Wo = params_phys.deltarho*params_phys.grav*...
+    params_phys.volume0/(2*pi*params_phys.sigma*params_phys.rneedle);
+
+% Dimensionless numbers for the paper
+params_phys.Wo_paper = Wo;
+params_phys.Ar_paper = Ar;
+
+
+
